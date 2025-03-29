90d2ab9344abb75cca170481756731289a05c67c
Anna Luiza <annaluizax67@gmail.com>
2025-03-29 17:38:34 -0300

    Creating PlayerShot Creating EnemyShot

--- code/Const.py
+++ code/Const.py
@@ -7,16 +7,28 @@ COLOR_YELLOW = (255, 255, 153)
 # E
 EVENT_ENEMY = pygame.USEREVENT + 1
 
-ENTITY_SPEED = {'Level1Bg0': 1,
-                'Level1Bg1': 4,
-                'Level1Bg2': 2,
-                'Level1Bg3': 3,
-                'Level1Bg4': 3,
-                'Player1': 3,
-                'Player2': 3,
-                'Enemy1': 2,
-                'Enemy2': 1,
-                }
+ENTITY_SPEED = {
+    'Level1Bg0': 0,
+    'Level1Bg1': 1,
+    'Level1Bg2': 2,
+    'Level1Bg3': 3,
+    'Level1Bg4': 4,
+    'Level1Bg5': 5,
+    'Level1Bg6': 6,
+    'Level2Bg0': 0,
+    'Level2Bg1': 1,
+    'Level2Bg2': 2,
+    'Level2Bg3': 3,
+    'Level2Bg4': 4,
+    'Player1': 3,
+    'Player1Shot': 1,
+    'Player2': 3,
+    'Player2Shot': 3,
+    'Enemy1': 1,
+    'Enemy1Shot': 5,
+    'Enemy2': 1,
+    'Enemy2Shot': 2,
+}
 
 ENTITY_HEALTH = {
     'Level1Bg0': 999,
@@ -39,6 +51,13 @@ ENTITY_HEALTH = {
     'Enemy1Shot': 1,
     'Enemy2': 60,
     'Enemy2Shot': 1,
+}
+
+ENTITY_SHOT_DELAY = {
+    'Player1': 20,
+    'Player2': 20,
+    'Enemy1': 100,
+    'Enemy2': 200,
 }
 
 # M
